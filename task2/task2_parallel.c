6af91d4964fa31529d937c23763242f7a3888812
ElizavetaEsyunina <144695505+ElizavetaEsyunina@users.noreply.github.com>
2025-06-09 00:00:21 +0500

    added task2_parallel.c

---
+++ task2/task2_parallel.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
+
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void fillArrayWithRandomNumbers(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    int array_size;
+    int *arr = NULL;
+    double start_time, end_time, time_taken;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) {
+        char *array_size_str = getenv("ARRAY_SIZE");
+        if (array_size_str == NULL) {
+            fprintf(stderr, "Переменная ARRAY_SIZE не установлена.\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        array_size = atoi(array_size_str);
+
+        if (array_size <= 0) {
+            fprintf(stderr, "Некорректное значение ARRAY_SIZE.\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        arr = (int *)malloc(array_size * sizeof(int));
+        if (arr == NULL) {
+            fprintf(stderr, "Не удалось выделить память под массив.\n");
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        srand(time(NULL));
+        fillArrayWithRandomNumbers(arr, array_size);
+
+        start_time = MPI_Wtime();
+        bubbleSort(arr, array_size);
+        end_time = MPI_Wtime();
+        time_taken = end_time - start_time;
+
+        printf("Время, затраченное на сортировку: %f секунд\n", time_taken);
+        printf("Количество процессов: %d\n", size);
+
+        free(arr);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
