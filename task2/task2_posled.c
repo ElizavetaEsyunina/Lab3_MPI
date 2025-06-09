6ebc45de1899fb4796ffda0b898552dc2b03a322
ElizavetaEsyunina <144695505+ElizavetaEsyunina@users.noreply.github.com>
2025-06-09 08:34:56 +0500

    Update task2_posled.c

--- task2/task2_posled.c
+++ task2/task2_posled.c
@@ -20,18 +20,31 @@ void fillArrayWithRandomNumbers(int arr[], int n) {
   }
 }
 
-#define NUM_RUNS 100
 int main() {
   char *array_size_str = getenv("ARRAY_SIZE");
+  char *num_iterations_str = getenv("NUM_ITERATIONS");
+  char *endptr; // Для strtol
+
   if (array_size_str == NULL) {
     fprintf(stderr, "Переменная окружения ARRAY_SIZE не установлена.\n");
     return 1;
   }
 
+  if (num_iterations_str == NULL) {
+    fprintf(stderr, "Переменная окружения NUM_ITERATIONS не установлена.\n");
+    return 1;
+  }
+  
   int array_size = atoi(array_size_str);
+  int num_iterations = strtol(num_iterations_str, &endptr, 10);
 
   if (array_size <= 0) {
     fprintf(stderr, "Некорректное значение ARRAY_SIZE.  Должно быть положительным целым числом.\n");
+    return 1;
+  }
+
+  if (*endptr != '\0' || num_iterations <= 0) {
+    fprintf(stderr, "Некорректное значение NUM_ITERATIONS. Должно быть положительным целым числом.\n");
     return 1;
   }
 
@@ -42,12 +55,22 @@ int main() {
   }
   
   srand(time(NULL));
-  fillArrayWithRandomNumbers(arr, array_size);
-  clock_t start_time = clock();
-  bubbleSort(arr, array_size);
-  clock_t end_time = clock();
-  double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-  printf("Время, затраченное на сортировку: %f секунд\n", time_taken);
+
+  double total_time = 0.0;
+  for (int i = 0; i < num_iterations; i++) {
+    fillArrayWithRandomNumbers(arr, array_size);
+    clock_t start_time = clock();
+    bubbleSort(arr, array_size);
+    clock_t end_time = clock();
+    double time_taken = (double)(end_time - start_time) / CLOCKS_PER_SEC;
+    total_time += time_taken;
+
+    printf("Итерация %d: Время, затраченное на сортировку: %f секунд\n", i + 1, time_taken);
+  }
+
+  double average_time = total_time / num_iterations;
+  printf("Среднее время, затраченное на сортировку: %f секунд\n", average_time);
+
   free(arr);
   return 0;
 }
